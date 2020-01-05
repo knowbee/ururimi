3fe0a673a6131cc422845bf9db3bc47855e0fad3
Googler <knowbeeinc@gmail.com>
2020-01-05 14:04:18 +0200

    reason

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
+"""
+Author: Igwaneza Bruce <knowbeeinc@gmail.com>
+Attempt to collect all kinyarwanda words with english translations from kinyarwanda dictionary,
+Source: kinyarwanda.net
+"""
+
+
 from bs4 import BeautifulSoup as bsf
 from urllib.request import urlopen
-from tqdm import tqdm  # progress bar library
+from tqdm import tqdm
 import time
 import json
 link = "http://kinyarwanda.net/index.php?q=%2A&start="
@@ -13,7 +20,8 @@ def app():
 
 def scrape(t, f):
     start = 0
-    while(start < 10):
+    end = 6168
+    while(start < end):
         with urlopen(t + str(start), data=None, timeout=1000) as page:
             soup = bsf(page, 'html.parser')
             words = soup.findAll("li", attrs={"class": "entry"})
@@ -22,12 +30,9 @@ def scrape(t, f):
                     time.sleep(0.1)
                     pbar.update(5)
                     pbar.desc = 'scraping'
-                    print(title.find("span", attrs={"class": "meaning"}).text)
                     f.update({title.find("span", attrs={"class": "lemma"}).text: title.find(
                         "span", attrs={"class": "meaning"}).text})
             start += 10
-    print(start)
-    print(f)
     print(f"'size': {len(f)}")
-    # with open('amagambo.json', 'w') as file:
-    #     json.dump(f, file)
+    with open('amagambo.json', 'w') as file:
+        json.dump(f, file)
